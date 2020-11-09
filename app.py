dfd6abfff6000c1b2af9c3bbc8bfb2bdbd5db19d
ajlongcoy21 <ajlongcoy21@gmail.com>
2020-11-08 18:48:21 -0600

    created register, login, and exit functions

--- app.py
+++ app.py
@@ -1,19 +1,107 @@
 # source ./env/bin/activate
 # deactivate
 
-from flask import Flask
+from flask import Flask, g, render_template, flash, redirect, url_for, abort
+from flask_bcrypt import check_password_hash
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+
+import forms
+import models
 
 DEBUG = True
 PORT = 8000
 HOST = '0.0.0.0'
 
 app = Flask(__name__)
+app.secret_key = 'asdf;lkajd23497@)(*@&$asfl;ajaf'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+
+@app.before_request
+def before_request():
+    """
+    Connect to the database before each request.
+    """
+    g.db = models.DATABASE
+    g.db.connect()
+    g.user = current_user
+
+@app.after_request
+def after_request(response):
+    """
+    Close the database connection after each request
+    """
+    g.db.close()
+    return response
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('layout.html')
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    """
+    docstring
+    """
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash("Yay, you registered!", "success")
+        models.User.create_user(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(models.User.email == form.email.data)
+        except models.DoesNotExist:
+            flash("Your email or password doesn't match!", "error")
+        else:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash("You've been logged in!", "success")
+                return redirect(url_for('index'))
+            else:
+                flash("Your email or password doesn't match!", "error")
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You've been logged out! Come back soon!", "success")
+    return redirect(url_for('index'))
+
+@app.route('/entries/<int:id>')
+def entries(id):
+    return render_template('detail.html')
 
 
 if __name__ == '__main__':
-    
+    models.initialize()
+    try:
+        models.User.create_user(
+            username='testuser',
+            email='testuser@gmail.com',
+            password='test',
+            admin=True
+        )
+    except ValueError:
+        pass
+
     app.run(debug=DEBUG, host=HOST, port=PORT)
---
+++ models.py
@@ -0,0 +1,56 @@
+import datetime
+
+from flask_login import UserMixin
+from flask_bcrypt import generate_password_hash, check_password_hash
+from peewee import *
+
+DATABASE = SqliteDatabase('journal.db')
+
+class User(UserMixin, Model):
+    id = IntegerField(primary_key=True)
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+    joined_at = DateTimeField(default=datetime.datetime.now)
+    is_admin = BooleanField(default=False)
+
+    class Meta:
+        database = DATABASE
+        order_by = ('-joined_at',)
+
+    def get_entries(self):
+        return Entry.select().where(Entry.user == self)
+
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        """
+        Create instance of a user in the database
+        """
+        try:
+            with DATABASE.transaction():
+                cls.create(
+                    username = username,
+                    email = email,
+                    password = generate_password_hash(password),
+                    is_admin = admin
+                )
+        except IntegrityError:
+            raise ValueError("User already exists")
+
+class Entry(Model):
+    id = IntegerField(primary_key=True)
+    timestamp = DateTimeField(default=datetime.datetime.now)
+    user = ForeignKeyField(
+        User,
+        related_name='entries'
+    )
+    content = TextField()
+
+    class Meta:
+        database = DATABASE
+        order_by = ('-timestamp',)
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User, Entry], safe=True)
+    DATABASE.close()
